3e97be59e8ca9525098792c2d42ff0c9c2ac02a8
AlexBoat <117096647+AlexBoat@users.noreply.github.com>
2022-12-22 19:09:09 +0200

    Add files via upload

---
+++ Hex_Viewer_/Hex_Viewer_/Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace Hex_Viewer_
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox3 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Arial Narrow", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(215, 15);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(186, 23);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "date.txt";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial Narrow", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(154, 16);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "File name:";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBox1.Location = new System.Drawing.Point(9, 53);
+            this.richTextBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(63, 305);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Text = "";
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBox2.Location = new System.Drawing.Point(76, 53);
+            this.richTextBox2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(337, 305);
+            this.richTextBox2.TabIndex = 3;
+            this.richTextBox2.Text = "";
+            // 
+            // richTextBox3
+            // 
+            this.richTextBox3.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBox3.Location = new System.Drawing.Point(417, 53);
+            this.richTextBox3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.richTextBox3.Name = "richTextBox3";
+            this.richTextBox3.Size = new System.Drawing.Size(209, 305);
+            this.richTextBox3.TabIndex = 4;
+            this.richTextBox3.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Arial", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(419, 10);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(65, 32);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "View";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(634, 369);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.richTextBox3);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form1";
+            this.Text = "Hex_Viewer";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.RichTextBox richTextBox3;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Hex_Viewer_/Hex_Viewer_/Form1.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Hex_Viewer_
+{
+    public partial class Form1 : Form
+    {
+        int contor_pt_endline = 0, contor_pt_index = 1;
+        string rezultat16 = "", line, contor_pt_index16 = "00000001";
+        int intreg_aux = 0;
+        int index_aux = 0;
+        string rezultat_index = "";
+        string file = "date.txt";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text = "00000001";
+            richTextBox2.Text = "";
+            richTextBox3.Text = "";
+            contor_pt_endline = 0;
+            contor_pt_index = 0;
+            rezultat16 = "";
+            contor_pt_index16 = "00000001";
+            intreg_aux = 0;
+            index_aux = 0;
+            rezultat_index = "";
+            richTextBox1.Width = 63;
+            richTextBox2.Width = 337;
+            richTextBox2.Left = 76;
+            richTextBox3.Width = 209;
+            richTextBox3.Left = 417;
+            file = textBox1.Text;
+            try
+            {
+
+                using (StreamReader sr = new StreamReader(file)) //Hex_viewer\Hex_viewer\bin\Debug\date.txt
+                {
+                    while (sr.Peek() >= 0)
+                    {
+                        line = sr.ReadLine();
+                        richTextBox3.Text += line;
+                        for (int i = 0; i < line.Length; i++)
+                        {
+                            rezultat_index = "";
+                            contor_pt_index16 = "";
+                            index_aux = 0;
+                            intreg_aux = 0;
+                            intreg_aux = Convert.ToInt32(line[i]);
+                            if (intreg_aux <= 9)
+                            {
+                                rezultat16 = "0" + Convert.ToString(intreg_aux);
+                            }
+                            else
+                            {
+                                while (intreg_aux != 0)
+                                {
+                                    switch (intreg_aux % 16)
+                                    {
+                                        case 10: rezultat16 = "A" + rezultat16; break;
+                                        case 11: rezultat16 = "B" + rezultat16; break;
+                                        case 12: rezultat16 = "C" + rezultat16; break;
+                                        case 13: rezultat16 = "D" + rezultat16; break;
+                                        case 14: rezultat16 = "E" + rezultat16; break;
+                                        case 15: rezultat16 = "F" + rezultat16; break;
+                                        default: rezultat16 = Convert.ToString(intreg_aux % 16) + rezultat16; break;
+                                    }
+                                    intreg_aux /= 16;
+                                }
+                            }
+                            richTextBox2.Text += rezultat16 + " ";
+                            contor_pt_endline++;
+                            if (contor_pt_endline == 16)
+                            {
+                                contor_pt_endline = 0;
+                                contor_pt_index++;
+                                if (contor_pt_index <= 9)
+                                {
+                                    contor_pt_index16 = "0000000" + Convert.ToString(contor_pt_index);
+                                    richTextBox1.Text += contor_pt_index16;
+                                }
+                                else
+                                {
+                                    contor_pt_index16 = "";
+                                    index_aux = contor_pt_index;
+                                    while (index_aux != 0)
+                                    {
+                                        switch (index_aux % 16)
+                                        {
+                                            case 10: rezultat_index = "A" + rezultat_index; break;
+                                            case 11: rezultat_index = "B" + rezultat_index; break;
+                                            case 12: rezultat_index = "C" + rezultat_index; break;
+                                            case 13: rezultat_index = "D" + rezultat_index; break;
+                                            case 14: rezultat_index = "E" + rezultat_index; break;
+                                            case 15: rezultat_index = "F" + rezultat_index; break;
+                                            default: rezultat_index = Convert.ToString(index_aux % 16) + rezultat_index; break;
+                                        }
+                                        index_aux /= 16;
+                                    }
+                                    if(rezultat_index=="15")
+                                    {
+                                        richTextBox1.Width = 79;
+                                        richTextBox2.Left = 92;
+                                        richTextBox2.Width = 354;
+                                        richTextBox3.Left = 447;
+                                        richTextBox3.Width = 174;
+                                    }
+                                    if(rezultat_index=="16")
+                                    {
+                                        richTextBox1.Width = 80;
+                                        richTextBox2.Left = 93;
+                                        richTextBox2.Width = 355;
+                                        richTextBox3.Left = 448;
+                                        richTextBox3.Width = 175;
+                                    }
+                                    for (int j = 1; j <= 8 - rezultat_index.Length; j++)
+                                    {
+                                        contor_pt_index16 += "0";
+                                    }
+                                    contor_pt_index16 += rezultat_index;
+                                    richTextBox1.Text += contor_pt_index16;
+                                }
+                            }
+                            rezultat16 = "";
+                        }
+                    }
+                }
+            }
+            catch(Exception E)
+            {
+                MessageBox.Show("Invalid input!");
+            }
+        }
+    }
+}
